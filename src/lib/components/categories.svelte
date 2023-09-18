abc0414c1633acdca34fd6f2169903e8ad5bb876
Michael Saa Olu Siyanbola Johnson <113233530+SOSJ001@users.noreply.github.com>
2023-09-18 02:13:44 +0000

    initial commit

---
+++ src/lib/components/categories.svelte
@@ -0,0 +1,9 @@
+<div class=" flex flex-row gap-10 text-gray-50 justify-center p-5">
+<a href="">Plant Pots</a>
+<a href="">Plant Pots</a>
+<a href="">Plant Pots</a>
+<a href="">Plant Pots</a>
+<a href="">Plant Pots</a>
+<a href="">Plant Pots</a>
+<a href="">Plant Pots</a>
+</div>
