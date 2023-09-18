0e7b081f0960962f51497b492d65524122127dba
Michael Saa Olu Siyanbola Johnson <113233530+SOSJ001@users.noreply.github.com>
2023-09-18 13:32:17 +0000

    initial commit

--- src/lib/components/listings.svelte
+++ src/lib/components/listings.svelte
@@ -1,3 +1,5 @@
-<div class="bg-gray-500">
-
+<div class="bg-gray-500 flex flex-row items-center p-10">
+    <div>hi</div>
+    <div>hi</div>
+    
 </div>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -2,6 +2,7 @@
     import Categories from "../lib/components/categories.svelte";
     import Different from "../lib/components/different.svelte";
     import Hero from "../lib/components/hero.svelte";
+    import Listings from "../lib/components/listings.svelte";
     import Top from "../lib/components/top.svelte";
 </script>
 
@@ -10,4 +11,5 @@
     <Categories />
     <Hero />
     <Different />
+    <Listings/>
 </div>
